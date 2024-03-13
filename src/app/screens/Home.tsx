8f3bcf8140809baa5e044740803a814d579b6d03
Cesar <cesar.alcoholado@hotmail.com>
2024-03-13 13:26:06 -0300

    added disclaimer

--- src/app/screens/Home.tsx
+++ src/app/screens/Home.tsx
@@ -8,8 +8,12 @@ const HomeScreen: React.FC = () => {
     <>
       <App />
       <Container alignSelf="center" maxWidth="container.xl" paddingX={0}>
-        <Text>{`<HomeScreen Test/>`}</Text>
+        {/* <Text>{`<HomeScreen Test/>`}</Text> */}
         <br />
+        <Text backgroundColor="yellow.300">
+          @Disclaimer: Esta pagina es un desafio, no tiene ninguna relacion con la empresa de
+          Mercado Libre, ni es un intento de estafa mediante Phishing
+        </Text>
         <Link href="/productId">Ver página de detalle de producto</Link>
       </Container>
     </>
--- src/product/screens/Details.tsx
+++ src/product/screens/Details.tsx
@@ -829,6 +829,10 @@ const DetailsScreen: React.FC<Props> = ({product = mock.product}) => {
           </Stack>
         </Container>
       </Container>
+      <Text backgroundColor="yellow.300">
+        @Disclaimer: Esta pagina es un desafio, no tiene ninguna relacion con la empresa de Mercado
+        Libre, ni es un intento de estafa mediante Phishing
+      </Text>
     </>
   );
 };
